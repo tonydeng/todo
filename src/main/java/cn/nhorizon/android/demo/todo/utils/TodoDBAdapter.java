cdecfb2b8a26a4149e135342587c186fdff88431
Tony Deng <taod@oupeng.com>
2014-02-02 16:31:20 +0800

    添加TodoDBAdapter

--- src/main/java/cn/nhorizon/android/demo/todo/utils/TodoDBAdapter.java
+++ src/main/java/cn/nhorizon/android/demo/todo/utils/TodoDBAdapter.java
@@ -11,63 +11,71 @@ public class TodoDBAdapter {
 	private static final String KEY_TASK = "task";
 	private static final String KEY_STATE = "state";
 	private static final String DATABASE_TABLE = "todolist";
-	
+
 	private Context context;
 	private SQLiteDatabase database;
 	private MySQLiteHelper dbHelper;
-	
-	public TodoDBAdapter(Context context){
+
+	public TodoDBAdapter(Context context) {
 		this.context = context;
 	}
-	
-	public TodoDBAdapter open(){
+
+	public TodoDBAdapter open() {
 		dbHelper = new MySQLiteHelper(context);
 		database = dbHelper.getWritableDatabase();
 		return this;
 	}
-	
-	public void close(){
+
+	public void close() {
 		dbHelper.close();
 	}
-	
-	public long createTask(String task, int completed){
+
+	public long createTask(String task, int completed) {
 		ContentValues values = new ContentValues();
 		values.put(KEY_TASK, task);
 		values.put(KEY_STATE, completed);
 		Log.d("Create Task DBTEST", values.toString());
 		return database.insert(DATABASE_TABLE, null, values);
 	}
-	
-	public boolean isTaskChecked(String task){
-		Cursor c = database.query(DATABASE_TABLE, new String[]{KEY_STATE}, KEY_STATE+"=?", new String[]{task}, null, null,null);
-		Log.d("is task checked DBTEST", ""+c.getInt(c.getColumnIndex(KEY_STATE)));
+
+	public boolean isTaskChecked(String task) {
+		Cursor c = database.query(DATABASE_TABLE, new String[] { KEY_STATE },
+				KEY_STATE + "=?", new String[] { task }, null, null, null);
+		Log.d("is task checked DBTEST",
+				"" + c.getInt(c.getColumnIndex(KEY_STATE)));
 		return c.getInt(c.getColumnIndex(KEY_STATE)) == 1;
 	}
-	
-	public long updateTask(String task, int completed){
+
+	public long updateTask(String task, int completed) {
 		ContentValues values = new ContentValues();
 		values.put(KEY_STATE, completed);
 		Log.d("update task DBTEST", values.toString());
-		return database.update(DATABASE_TABLE, values, KEY_TASK+"=?", new String[]{task});
+		return database.update(DATABASE_TABLE, values, KEY_TASK + "=?",
+				new String[] { task });
 	}
-	
-	public long deleteDB(){
+
+	public long deleteDB() {
 		return database.delete(DATABASE_TABLE, null, null);
 	}
-	
-	public long deleteTask(String task){
-		return database.delete(DATABASE_TABLE, KEY_TASK+"=?", new String[]{task});
+
+	public long deleteTask(String task) {
+		return database.delete(DATABASE_TABLE, KEY_TASK + "=?",
+				new String[] { task });
 	}
-	
-	public long deleteCompleteTasks(){
-		return database.delete(DATABASE_TABLE, KEY_STATE+"=?", new String[]{"1"});
+
+	public long deleteCompleteTasks() {
+		return database.delete(DATABASE_TABLE, KEY_STATE + "=?",
+				new String[] { "1" });
 	}
-	
-	public Cursor fetchAllTasks(){
-		return database.query(DATABASE_TABLE, new String[]{KEY_ROWID, KEY_TASK, KEY_STATE}, null, null, null, null, null);
+
+	public Cursor fetchAllTasks() {
+		return database.query(DATABASE_TABLE, new String[] { KEY_ROWID,
+				KEY_TASK, KEY_STATE }, null, null, null, null, null);
 	}
-	
-	public Cursor fetchTask(String task){
-		return database.query(DATABASE_TABLE, new String[]{KEY_ROWID, KEY_TASK, KEY_STATE}, KEY_TASK+"=?", new String[]{task}, null, null, null);
+
+	public Cursor fetchTask(String task) {
+		return database.query(DATABASE_TABLE, new String[] { KEY_ROWID,
+				KEY_TASK, KEY_STATE }, KEY_TASK + "=?", new String[] { task },
+				null, null, null);
 	}
 }
